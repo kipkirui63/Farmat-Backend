af75f587deffc9e2e58025b90ecbfd03c0a6a166
kipkirui63 <enocksang8356@gmail.com>
2023-11-24 13:00:12 +0300

    defined namespace and named the api

--- api/api_models.py
+++ api/api_models.py
@@ -1,4 +1,11 @@
+from api import fields, api, ma, CartItem
+from marshmallow import Schema, fields
+from flask_restx import Api,Resource,Namespace,fields
 
+ns = Namespace('farmart')
+api.add_namespace(ns)
+
+# ------------------------- A P I _ M O D E L S ------------------------
 
 
 
